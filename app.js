1b63940f06dec666573b8c6de844043a1a266f3f
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2025-04-07 11:16:12 +0200

    implement cleaner approach for drawing board

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const board = document.getElementById('board');
 
 const BOARD_HEIGHT = 8;
 const BOARD_WIDTH = 8;
-const INFO_LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
 const isWhite = (row, column) => {
   if (row % 2 !== 0) {
@@ -18,67 +17,152 @@ const isWhite = (row, column) => {
   }
 } 
 
-const drawBoard = () => {
+const drawBoardInfoColumn = (isLeft) => {
+  const boardInfoColumn = document.createElement('div');
+  boardInfoColumn.className = 'board-info-column';
+  boardInfoColumn.classList.add(
+    `board-info-column-${isLeft ? 'left' : 'right'}`
+  );
+  for (let i = 0; i < BOARD_HEIGHT ; i++) {
+    const boardInfoColumnBox = document.createElement('div');
+    boardInfoColumnBox.className = 'board-info-column-box';
+    boardInfoColumnBox.textContent = i + 1;
+    boardInfoColumn.append(boardInfoColumnBox);
+  }
+  return boardInfoColumn;
+}
+
+const drawBoardCorner = () => {
+  const boardCorner = document.createElement('div');
+  boardCorner.className = 'board-corner';
+  return boardCorner;
+}
+
+const drawBoardInfoRow = (isTop) => {
+  const boardInfoRow = document.createElement('div');
+  boardInfoRow.className = 'board-info-row';
   
+  
+  boardInfoRow.appendChild(drawBoardCorner());
+  for (let i = 0; i < BOARD_WIDTH; i++) {
+    const boardInfoRowBox = document.createElement('div');
+    boardInfoRowBox.className = 'board-info-row-box';
+    boardInfoRowBox.classList.add(
+      `board-info-row-box-${isTop ? 'top' : 'bottom'}`
+    );
+    // 65 is char code for letter 'A'
+    boardInfoRowBox.textContent = String.fromCharCode(65 + i);
+    boardInfoRow.appendChild(boardInfoRowBox);
+  }
+  boardInfoRow.appendChild(drawBoardCorner());
 
+  return boardInfoRow;
+}
 
-  for (let i = 0; i < BOARD_HEIGHT + 2; i++) {
+const drawBoard = () => {
+  const boardContainer = document.createElement('div');
+  boardContainer.id = 'board-container';
+  boardContainer.className = 'board-container';
 
-
-
+  for (let i = 0 ; i < BOARD_HEIGHT ; i++) {
+    
     const boardRow = document.createElement('div');
+    boardRow.className = 'board-row';
     
-    if (i == 0 || i == BOARD_HEIGHT + 1) {
-      boardRow.className = 'board-row-info';
-    } else {
-      boardRow.className = 'board-row';
+    for (let j = 0 ; j < BOARD_WIDTH ; j++) {
+      const boardButton = document.createElement('button');
+      boardButton.id = `board-button-${j}-${i}` // (x - row, y - column)
+      boardButton.className = 'board-button';
+      if (isWhite(i, j)) {
+        boardButton.classList.add('board-white');
+      } else {
+        boardButton.classList.add('board-black');
+      }
+      boardButton.addEventListener('click', () => {
+        console.log(`(${j},${i})`);
+      })
+      boardRow.appendChild(boardButton);
     }
-    
-    
-    for (let j = 0; j < BOARD_WIDTH + 2; j++) {
+    boardContainer.appendChild(boardRow);
+  }
+  return boardContainer;
+}
 
-      if (i == 0 || i == BOARD_WIDTH + 1) {
-        const boardBoxInfo = document.createElement('div');
+const drawBoardWithInfo = () => {
+  
+  
+  const boardWithInfoColumns = document.createElement('div');
+  boardWithInfoColumns.className = 'board-with-info-columns';
+
+  boardWithInfoColumns.append(drawBoardInfoColumn(true));
+  boardWithInfoColumns.appendChild(drawBoard());
+  boardWithInfoColumns.append(drawBoardInfoColumn(false));
+
+  board.appendChild(drawBoardInfoRow(true));
+  board.appendChild(boardWithInfoColumns);
+  board.appendChild(drawBoardInfoRow(false));
+}
+
+
+
+
+// const drawBoardNotOptimal = () => {
+//   for (let i = 0; i < BOARD_HEIGHT + 2; i++) {
+//     const boardRow = document.createElement('div');
+    
+//     if (i == 0 || i == BOARD_HEIGHT + 1) {
+//       boardRow.className = 'board-row-info';
+//     } else {
+//       boardRow.className = 'board-row';
+//     }
+    
+    
+//     for (let j = 0; j < BOARD_WIDTH + 2; j++) {
+
+//       if (i == 0 || i == BOARD_WIDTH + 1) {
+//         const boardBoxInfo = document.createElement('div');
         
 
-        if (j > 0 && j < BOARD_WIDTH + 1 ) {
-          boardBoxInfo.textContent = INFO_LETTERS[j-1];
-          boardBoxInfo.className = 'board-box-info-row';
-        } else {
-          boardBoxInfo.className = 'board-box-info-corner';
-        }
-        // if (isWhite(i, j)) {
-        //   boardBoxInfo.classList.add('white');
-        // } else {
-        //   boardBoxInfo.classList.add('black');
-        // }
-        boardRow.appendChild(boardBoxInfo);
-      } else {
-        const boardBox = document.createElement('div');
+//         if (j > 0 && j < BOARD_WIDTH + 1 ) {
+//           boardBoxInfo.textContent = INFO_LETTERS[j-1];
+//           boardBoxInfo.className = 'board-box-info-row';
+//         } else {
+//           boardBoxInfo.className = 'board-box-info-corner';
+//         }
+//         // if (isWhite(i, j)) {
+//         //   boardBoxInfo.classList.add('white');
+//         // } else {
+//         //   boardBoxInfo.classList.add('black');
+//         // }
+//         boardRow.appendChild(boardBoxInfo);
+//       } else {
+//         const boardBox = document.createElement('div');
 
-        if (j > 0 && j < BOARD_WIDTH + 1 ) {
-          boardBox.className = 'board-box';
-          boardBox.id = `board-box-${i}-${j}`;
-          if (isWhite(i, j)) {
-            boardBox.classList.add('white');
-          } else {
-            boardBox.classList.add('black');
-          }
-        } else {
-          boardBox.textContent = BOARD_WIDTH + 1 - i;
-          boardBox.className = 'board-box-info-column';
-        }
+//         if (j > 0 && j < BOARD_WIDTH + 1 ) {
+//           boardBox.className = 'board-box';
+//           boardBox.id = `board-box-${i}-${j}`;
+//           if (isWhite(i, j)) {
+//             boardBox.classList.add('white');
+//           } else {
+//             boardBox.classList.add('black');
+//           }
+//         } else {
+//           boardBox.textContent = BOARD_WIDTH + 1 - i;
+//           boardBox.className = 'board-box-info-column';
+//         }
 
-        boardRow.appendChild(boardBox);
-      }
+//         boardRow.appendChild(boardBox);
+//       }
 
       
 
-    }
+//     }
 
-    board.appendChild(boardRow);
-  }
+//     board.appendChild(boardRow);
+//   }
 
-}
+// }
 
-drawBoard();
+// drawBoardNotOptimal();
+
+drawBoardWithInfo();
