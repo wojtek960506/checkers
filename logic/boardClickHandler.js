ca2fb63c8bf04f8fa13edeb8881e0a5eb7a9f6f0
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2025-04-08 12:35:14 +0200

    final spliting of handle logic

--- logic/boardClickHandler.js
+++ logic/boardClickHandler.js
@@ -3,44 +3,15 @@ import { cancelMove } from "./cancelMove.js";
 import { executeMove } from "./executeMove.js";
 import { getBoardButtonCoordinates } from "../utils.js";
 import { hasClass } from "../utils.js";
-import { getMoves } from "./getMoves.js";
+import { handleBeforeMoveBlack, handleBeforeMoveWhite } from "./handleBeforeMove.js";
 
 
 
 
 
 
-const handleBeforeMove = (boardElem, board, clickedBox, isWhite) => {
-  const [clickedBoxX, clickedBoxY] = getBoardButtonCoordinates(clickedBox.id);
-  
-  const { possibleMoves, possibleBeatings } = getMoves(
-    board, clickedBoxX, clickedBoxY, isWhite
-  );
 
-  boardElem.setAttribute('possible-moves', JSON.stringify(possibleMoves));
-  boardElem.setAttribute('possible-beatings', JSON.stringify(possibleBeatings));
 
-  for (let move of possibleMoves) {
-    const b = document.getElementById(`board-button-${move[0]}-${move[1]}`);
-    b.classList.add('pink');
-  }
-
-  for (let beating of possibleBeatings) {
-    const b = document.getElementById(`board-button-${beating[0]}-${beating[1]}`);
-    b.classList.add('red');
-  }
-  
-  boardElem.setAttribute('move-state', 'during-move');
-  boardElem.setAttribute('current-pawn', JSON.stringify([clickedBoxX,clickedBoxY]));
-}
-
-const handleBeforeMoveBlack = (boardElem, board, clickedBox) => {
-  handleBeforeMove(boardElem, board, clickedBox, false);
-}
-
-const handleBeforeMoveWhite = (boardElem, board, clickedBox) => {
-  handleBeforeMove(boardElem, board, clickedBox, true);
-}
 
 
 
---
+++ logic/handleBeforeMove.js
@@ -0,0 +1,37 @@
+import { getBoardButtonCoordinates } from "../utils.js";
+import { getMoves } from "./getMoves.js";
+
+const handleBeforeMove = (boardElem, board, clickedBox, isWhite) => {
+  const [clickedBoxX, clickedBoxY] = getBoardButtonCoordinates(clickedBox.id);
+  
+  const { possibleMoves, possibleBeatings } = getMoves(
+    board, clickedBoxX, clickedBoxY, isWhite
+  );
+
+  // mark possible moves on board
+  for (let move of possibleMoves) {
+    const b = document.getElementById(`board-button-${move[0]}-${move[1]}`);
+    b.classList.add('pink');
+  }
+
+  // mark possible beatings on board
+  for (let beating of possibleBeatings) {
+    const b = document.getElementById(`board-button-${beating[0]}-${beating[1]}`);
+    b.classList.add('red');
+  }
+ 
+  boardElem.setAttribute('possible-moves', JSON.stringify(possibleMoves));
+  boardElem.setAttribute('possible-beatings', JSON.stringify(possibleBeatings));
+  boardElem.setAttribute('move-state', 'during-move');
+  boardElem.setAttribute('current-pawn', JSON.stringify([clickedBoxX,clickedBoxY]));
+}
+
+const handleBeforeMoveBlack = (boardElem, board, clickedBox) => {
+  handleBeforeMove(boardElem, board, clickedBox, false);
+}
+
+const handleBeforeMoveWhite = (boardElem, board, clickedBox) => {
+  handleBeforeMove(boardElem, board, clickedBox, true);
+}
+
+export { handleBeforeMoveBlack, handleBeforeMoveWhite };
